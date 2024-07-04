43b059c40cd88c68846d4e6f230da837a829d5b6
Harisanjay403 <168261655+Harisanjay403@users.noreply.github.com>
2024-07-04 12:59:56 +0530

    Update app.jsx
    finished

--- app.jsx
+++ app.jsx
@@ -5,8 +5,31 @@ export default function App() {
     const[includeLowerCase,setIncludeLowerCase]=useState(true)
     const[includeNumber,setIncludeNumber]=useState(true)
     const[includeSymbol,setIncludeSymbol]=useState(true)
-    const handleLength=(e)=>{
+
+    const [password,setPassword]=useState("")
+    
+    const generatePassword=()=>{
+        let charset = "";
+        if(includeUpperCase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (includeLowerCase) charset += "abcdefghijklmnopqrstuvwxyz";
+        if (includeNumber) charset += "0123456789";
+        if (includeSymbol) charset += "!@#$%^&*()";
+
+        let geneatedPassword="";
+        for(let i=0; i<=length; i++){
+            const randomnumber=Math.floor(Math.random()*charset.length)
+            geneatedPassword += charset[randomnumber]            
+        }
+        setPassword(geneatedPassword)
+        
+    }
+    
+        const handleLength=(e)=>{
         setLength(e.target.value)
+    }
+    const copyPassword=()=>{
+        navigator.clipboard.writeText(password)
+        alert(`password is copied your password id ${password}`)
     }
   return (
       <>
@@ -19,30 +42,37 @@ export default function App() {
               </div>
 
                <div className="checkbox-group">
-                   <input type="checkbox" id="upper"  checked={includeUpperCase}/>
+                   <input type="checkbox" id="upper"  checked={includeUpperCase} 
+                       onChange={(e)=> setIncludeUpperCase(e.target.checked)}/>
                    <label htmlFor="upper"> Include Upper Case</label>
                </div>
 
               <div className="checkbox-group">
-                   <input type="checkbox" id="lower" checked={includeLowerCase}/>
+                   <input type="checkbox" id="lower" checked={includeLowerCase}
+                       onChange={(e)=> setIncludeLowerCase(e.target.checked)}
+                       />
                    <label htmlFor="lower"> Include Lower Case</label>
                </div>
 
               <div className="checkbox-group">
-                   <input type="checkbox" id="number" checked={includeNumber}/>
+                   <input type="checkbox" id="number" checked={includeNumber}
+                       onChange={(e)=> setIncludeNumber(e.target.checked)}
+                       />
                    <label htmlFor="number"> Include Number</label>
                </div>
 
               <div className="checkbox-group">
-                   <input type="checkbox" id="symbol" checked={includeSymbol}/>
+                   <input type="checkbox" id="symbol" checked={includeSymbol}
+                       onChange={(e)=> setIncludeSymbol(e.target.checked)}
+                       />
                    <label htmlFor="symbol"> Include Symbol</label>
                </div>
 
-              <button className="generate-btn">Generate Password</button>
+              <button className="generate-btn" onClick={generatePassword}>Generate Password</button>
 
               <div className="genetated-password">
-                  <input type="text" readOnly />
-                  <button className="copy-btn">Copy </button>
+                  <input type="text" readOnly value={password} />
+                  <button className="copy-btn" onClick={copyPassword}>Copy</button>
               </div>
               
           </div>
